dc9298725fd9b5277661d44d48f566975787b735
aledelmo <delmonte.ale92@gmail.com>
2020-10-04 15:40:30 +0200

    PCA first graphs

--- PCA.R
+++ PCA.R
@@ -1,2 +1,24 @@
-ds <- read.csv('measurements.csv', sep=';')
-ds = as.numeric(ds)
+library(factoextra)
+
+ds <- na.omit(read.csv('measurements.csv', sep=';', dec=","))
+measurements = ds[,4:length(ds)]
+summary(measurements)
+res.pca <- prcomp(measurements, scale = TRUE)
+fviz_eig(res.pca)
+
+fviz_pca_ind(res.pca,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+fviz_pca_var(res.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+fviz_pca_biplot(res.pca, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
