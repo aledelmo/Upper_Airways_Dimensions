65feb92924acc2f47e5e366a700b9c365f02affe
aledelmo <delmonte.ale92@gmail.com>
2020-10-05 16:09:58 +0200

    paper reproduction

--- PCA.R
+++ PCA.R
@@ -1,20 +1,32 @@
+library(ggplot2)
+library(hrbrthemes)
 library(factoextra)
+library("RColorBrewer")
+
+rm(list = ls(all.names = TRUE)) 
+gc()
+
+# Dataset ----
 
 ds <- na.omit(read.csv('measurements.csv', sep=';', dec=","))
 measurements = ds[,4:length(ds)]
 summary(measurements)
+
+# PCA ----
+
 res.pca <- prcomp(measurements, scale = TRUE)
+
 fviz_eig(res.pca)
 
 fviz_pca_ind(res.pca,
              col.ind = "cos2", # Color by the quality of representation
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             gradient.cols = rev(brewer.pal(n = nrow(measurements), name = "Spectral")),
              repel = TRUE     # Avoid text overlapping
 )
 
 fviz_pca_var(res.pca,
              col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             gradient.cols = rev(brewer.pal(n = nrow(measurements), name = "Spectral")),
              repel = TRUE     # Avoid text overlapping
 )
 
@@ -22,3 +34,46 @@ fviz_pca_biplot(res.pca, repel = TRUE,
                 col.var = "#2E9FDF", # Variables color
                 col.ind = "#696969"  # Individuals color
 )
+
+
+# Data Series ----
+
+age = measurements[,1]
+ap_glottic_diameter = measurements[,9]
+ap_cricoid_diameter = measurements[,10]
+
+# Glottic Model ----
+
+model_glottic <- lm(ap_glottic_diameter ~ poly(age,3))
+#summary(model_glottic)
+#confint(model_glottic, level=0.95)
+
+predicted_intervals <- predict(model_glottic,data.frame(x=age),interval='confidence', level=0.99)
+measurements_predict = cbind(measurements, predicted_intervals)
+
+p <- (ggplot(measurements_predict, aes(x=age.en.mois, y=antéro.post.CV)) + geom_point() + 
+        geom_line(aes(y = fit), col='blue',size = 1) + geom_ribbon( aes(ymin = lwr, ymax = upr, colour = 'gray'), alpha = .15, linetype = 0)
+      + theme_ipsum(grid=FALSE, axis=TRUE, axis_title_just='c') + 
+        labs(x="Age (months)", y="Diameter (mm)", title="Anteroposterior Glottic Diameters", caption=paste("AIC", AIC(model_glottic))) +
+        ylim(0, 25) + guides(colour=FALSE))
+print(p)
+
+ggsave('/Users/imag2/Desktop/Upper_Airways_Dimensions/results/glottic.png', p)
+
+# Cricoid Model ----
+
+model_cricoid <- lm(ap_cricoid_diameter ~ poly(age,3))
+#summary(model_cricoid)
+#confint(model_cricoid, level=0.95)
+
+predicted_intervals <- predict(model_cricoid,data.frame(x=age),interval='confidence', level=0.99)
+measurements_predict = cbind(measurements, predicted_intervals)
+
+p <- (ggplot(measurements_predict, aes(x=age.en.mois, y=AP.cricoïde)) + geom_point() + 
+        geom_line(aes(y = fit), col='blue',size = 1) + geom_ribbon( aes(ymin = lwr, ymax = upr, colour = 'gray'), alpha = .15, linetype = 0)
+      + theme_ipsum(grid=FALSE, axis=TRUE, axis_title_just='c') + 
+        labs(x="Age (months)", y="Diameter (mm)", title="Anteroposterior Glottic Diameters", caption=paste("AIC", AIC(model_cricoid))) +
+        ylim(0, 25) + guides(colour=FALSE))
+print(p)
+
+ggsave('/Users/imag2/Desktop/Upper_Airways_Dimensions/results/cricoid.png', p)
