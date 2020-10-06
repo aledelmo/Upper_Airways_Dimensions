cfa63f3befcabd5d8e2bccbce596e8768f45cd9d
aledelmo <vyMquv-bugxeh-4suvwa>
2020-10-06 17:45:20 +0200

    train/test set split

--- Polynomial_Regression.R
+++ Polynomial_Regression.R
@@ -11,23 +11,36 @@ gc()
 ds <- na.omit(read.csv('measurements.csv', sep=';', dec=","))
 measurements = ds[,4:length(ds)]
 
-age = measurements[,1]
-ap_glottic_diameter = measurements[,9]
-ap_cricoid_diameter = measurements[,10]
+smp_size <- floor(0.8 * nrow(measurements))
+set.seed(42)
+
+train_ind <- sample(seq_len(nrow(measurements)), size = smp_size)
+train <- measurements[train_ind, ]
+test <- measurements[-train_ind, ]
+
+age_train = train[,1]
+age_test = test[,1]
 
 # Glottic Model ----
 
-model_glottic <- lm(ap_glottic_diameter ~ poly(age,3))
-#summary(model_glottic)
-#confint(model_glottic, level=0.95)
+ap_glottic_diameter_train = train[,9]
+ap_glottic_diameter_test = test[,9]
 
+age <- age_train
+model_glottic <- lm(ap_glottic_diameter_train ~ poly(age,3))
+
+age <- age_test
 predicted_intervals <- predict(model_glottic,data.frame(x=age),interval='confidence', level=0.99)
-measurements_predict = cbind(measurements, predicted_intervals)
+measurements_predict = cbind(test, predicted_intervals)
+
+d = ap_glottic_diameter_test-predicted_intervals[,1]
+mse_glottic = mean((d)^2)
+mae_glottic = mean(abs(d))
+rmse_glottic = sqrt(mse_glottic)
 
 p <- (ggplot(measurements_predict, aes(x=age.en.mois, y=antéro.post.CV)) + geom_point() + 
         geom_line(aes(y = fit), col='blue',size = 1) + geom_ribbon( aes(ymin = lwr, ymax = upr, colour = 'gray'), alpha = .15, linetype = 0)
-      + theme_ipsum(grid=FALSE, axis=TRUE, axis_title_just='c') + 
-        labs(x="Age (months)", y="Diameter (mm)", title="Anteroposterior Glottic Diameters", caption=paste("AIC", AIC(model_glottic))) +
+      + labs(x="Age (months)", y="Diameter (mm)", title="Anteroposterior Glottic Diameters", caption=paste("AIC", AIC(model_glottic))) +
         ylim(0, 25) + guides(colour=FALSE))
 print(p)
 
@@ -35,17 +48,26 @@ ggsave('/Users/imag2/Desktop/Upper_Airways_Dimensions/results/glottic.png', p)
 
 # Cricoid Model ----
 
-model_cricoid <- lm(ap_cricoid_diameter ~ poly(age,3))
+ap_cricoid_diameter_train = train[,10]
+ap_cricoid_diameter_test = test[,10]
+
+age <- age_train
+model_cricoid <- lm(ap_cricoid_diameter_train ~ poly(age,3))
 #summary(model_cricoid)
 #confint(model_cricoid, level=0.95)
 
+age <- age_test
 predicted_intervals <- predict(model_cricoid,data.frame(x=age),interval='confidence', level=0.99)
-measurements_predict = cbind(measurements, predicted_intervals)
+measurements_predict = cbind(test, predicted_intervals)
+
+d = ap_cricoid_diameter_test-predicted_intervals[,1]
+mse_cricoid = mean((d)^2)
+mae_cricoid = mean(abs(d))
+rmse_cricoid = sqrt(mse_glottic)
 
 p <- (ggplot(measurements_predict, aes(x=age.en.mois, y=AP.cricoïde)) + geom_point() + 
         geom_line(aes(y = fit), col='blue',size = 1) + geom_ribbon( aes(ymin = lwr, ymax = upr, colour = 'gray'), alpha = .15, linetype = 0)
-      + theme_ipsum(grid=FALSE, axis=TRUE, axis_title_just='c') + 
-        labs(x="Age (months)", y="Diameter (mm)", title="Anteroposterior Glottic Diameters", caption=paste("AIC", AIC(model_cricoid))) +
+      + labs(x="Age (months)", y="Diameter (mm)", title="Anteroposterior Glottic Diameters", caption=paste("AIC", AIC(model_cricoid))) +
         ylim(0, 25) + guides(colour=FALSE))
 print(p)
 
